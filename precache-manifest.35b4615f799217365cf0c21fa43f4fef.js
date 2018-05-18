08a60e423cc87659a7bdeb26b00e5be71cd5fbd4
Adam Rybinski <adam.rybinski@pl.ibm.com>
2018-05-18 21:41:04 +0200

    Updates

---
+++ precache-manifest.35b4615f799217365cf0c21fa43f4fef.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = [
+  {
+    "revision": "f483f8a068e7dff288ca",
+    "url": "/emoflow/static/js/main.f483f8a0.js"
+  },
+  {
+    "revision": "f483f8a068e7dff288ca",
+    "url": "/emoflow/static/css/main.65027555.css"
+  },
+  {
+    "revision": "0e3873bec2cd9768ecfc5d9214285112",
+    "url": "/emoflow/index.html"
+  }
+];
